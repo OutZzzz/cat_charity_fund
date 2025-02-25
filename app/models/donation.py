7a62ec88f11c70bce755a9976d321d9f905b3d87
Alexey Sudakov <outzz@yandex.ru>
2025-02-25 21:28:16 +0300

    Add: Добавлена модель Donation

---
+++ app/models/donation.py
@@ -0,0 +1,15 @@
+from sqlalchemy import (
+    Column, Text, Integer,
+    Boolean, DateTime, ForeignKey)
+
+from app.core.db import Base
+
+
+class Donation(Base):
+    user_id = Column(Integer, ForeignKey('user.id'))
+    comment = Column(Text, nullable=True)
+    full_amount = Column(Integer)
+    invested_amount = Column(Integer)
+    fully_invested = Column(Boolean)
+    create_date = Column(DateTime)
+    close_date = Column(DateTime)
