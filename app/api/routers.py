c5bf80c4e87122759e08535ab99b1ced152c940c
Alexey Sudakov <outzz@yandex.ru>
2025-02-26 18:05:36 +0300

    Add: добавлен роутер для эндпоинта charity_project

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import user_router
+from app.api.endpoints import user_router, charity_router
 
 
 main_router = APIRouter()
 main_router.include_router(user_router)
+main_router.include_router(
+    charity_router, prefix='/charity_project', tags=['charity_project']
+)
