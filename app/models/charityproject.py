5a08eed5a3ec8312230a8a8287a7c866d4a1f5b7
Alexey Sudakov <outzz@yandex.ru>
2025-02-26 16:52:45 +0300

    Fix: удален лишний код

--- app/models/charityproject.py
+++ app/models/charityproject.py
@@ -3,16 +3,6 @@ from sqlalchemy import Column, Text, String
 from app.models.base import BaseDonationModel
 
 
-""" class CharityProject(Base):
-    name = Column(String(100), unique=True, nullable=False)
-    description = Column(Text, nullable=False)
-    full_amount = Column(Integer, nullable=False)
-    invested_amount = Column(Integer, nullable=False)
-    fully_invested = Column(Boolean, nullable=False)
-    create_date = Column(DateTime, nullable=False)
-    close_date = Column(DateTime, nullable=True) """
-
-
 class CharityProject(BaseDonationModel):
     name = Column(String(100), unique=True, nullable=False)
     description = Column(Text, nullable=False)
