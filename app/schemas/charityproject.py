4f73a54b54ae08f2a5daf3212f75d1df87a1cf7a
Alexey Sudakov <outzz@yandex.ru>
2025-02-27 10:44:58 +0300

    Add: Написаны схемы

--- app/schemas/charityproject.py
+++ app/schemas/charityproject.py
@@ -1,8 +1,30 @@
-from pydantic import Field
+from datetime import datetime
+from typing import Optional
 
-from .base import ProjectBase
+from pydantic import Field, BaseModel
 
 
-class CharityProjectGet(ProjectBase):
+class CharityProjectCreate(BaseModel):
     name: str = Field(..., min_length=1, max_length=100)
     description: str = Field(..., min_length=1)
+    full_amount: int = Field(..., gt=0)
+
+
+class CharityProjectUpdate(BaseModel):
+    name: Optional[str] = Field(..., min_length=1, max_length=100)
+    description: Optional[str] = Field(..., min_length=1)
+    full_amount: Optional[int] = Field(..., gt=0)
+
+
+class CharityProjectDB(BaseModel):
+    name: str = Field(..., max_length=100)
+    description: str
+    full_amount: int = Field(..., gt=0)
+    id: int
+    invested_amount: int
+    fully_invested: bool
+    create_date: datetime
+    close_date: Optional[datetime]
+
+    class Config:
+        orm_mode = True
--- app/schemas/donation.py
+++ app/schemas/donation.py
@@ -1,8 +1,26 @@
+from datetime import datetime
 from typing import Optional
 
-from .base import ProjectBase
+from pydantic import Field, BaseModel
 
 
-class DonationBaseGetAll(ProjectBase):
-    user_id: int
+class DonationCreate(BaseModel):
+    full_amount: int = Field(..., gt=0)
     comment: Optional[str]
+
+
+class DonationUserDB(BaseModel):
+    full_amount: int = Field(..., gt=0)
+    id: int
+    comment: Optional[str]
+    create_date: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class DonationSuperUserDB(DonationUserDB):
+    user_id: int
+    invested_amount: int
+    fully_invested: bool
+    close_date: datetime
