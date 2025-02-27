7e491ce46a38b5ca50db73f7ec172c780ba81b89
Alexey Sudakov <outzz@yandex.ru>
2025-02-27 10:46:21 +0300

    Add: добавлен роутер для donation

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import user_router, charity_router
+from app.api.endpoints import user_router, charity_router, donation_router
 
 
 main_router = APIRouter()
-main_router.include_router(user_router)
 main_router.include_router(
-    charity_router, prefix='/charity_project', tags=['charity_project']
+    charity_router, prefix='/charity_project', tags=['Charity_Project']
 )
+main_router.include_router(
+    donation_router, prefix='/donation', tags=['Donation']
+)
+main_router.include_router(user_router)
