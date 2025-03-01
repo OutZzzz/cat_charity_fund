3fce8dafa682ac3d045fd3e171736849e08cd165
Alexey Sudakov <outzz@yandex.ru>
2025-03-01 14:46:14 +0300

    Add: validators

--- app/api/validators.py
+++ app/api/validators.py
@@ -5,16 +5,59 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.charity_projects import charity_project_crud
 from app.models import CharityProject
+from app.schemas.charityproject import CharityProjectCreate, CharityProjectUpdate
 
 
 async def check_project_exists(
-        project_id: int,
-        session: AsyncSession
+    project_id: int, session: AsyncSession
 ) -> CharityProject:
     project = await charity_project_crud.get(project_id, session)
     if project is None:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail='Проект не найден!'
+            status_code=HTTPStatus.NOT_FOUND, detail="Проект не найден!"
         )
     return project
+
+
+async def check_name_unique(
+        project: CharityProjectCreate,
+        session: AsyncSession):
+    project_name = await charity_project_crud.get_obj_with_same_name(
+        project,
+        session)
+    if project_name is not None:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail="Проект с таким именем уже существует.",
+        )
+
+
+def check_proj_for_delete(project: CharityProject):
+    if project.fully_invested or (project.invested_amount != 0):
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail=(
+                f"Нельзя удалять закрытый проект или проект, "
+                f"в который уже были инвестированы деньги"
+            ),
+        )
+
+
+def check_proj_for_update(project: CharityProject):
+    if project.fully_invested:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail=(
+                f'Закрытый проект нельзя редактировать'
+            )
+        )
+
+
+def check_value(project: CharityProject, updade_data: CharityProjectUpdate):
+    if project.invested_amount > updade_data.full_amount:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail=(
+                f'Нельзя установить требуемую сумму меньше уже вложенной.'
+            )
+        )
