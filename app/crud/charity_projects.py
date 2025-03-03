43c599e90f8ed9713a06f1b9222c9f6c88ba21fb
Alexey Sudakov <outzz@yandex.ru>
2025-03-03 13:01:56 +0300

    Fix: исправлены функции

--- app/crud/charity_projects.py
+++ app/crud/charity_projects.py
@@ -1,47 +1,11 @@
-from datetime import datetime
-
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from fastapi.encoders import jsonable_encoder
 
 from app.models import CharityProject
-from app.schemas import CharityProjectCreate
 from .base import CRUDBase
 
 
 class CRUDCharityProject(CRUDBase):
-
-    async def delete(
-            self,
-            db_obj,
-            session: AsyncSession,
-    ):
-        await session.delete(db_obj)
-        await session.commit()
-        return db_obj
-
-    async def update(
-            self,
-            db_obj,
-            obj_in,
-            session: AsyncSession
-    ):
-        obj_data = jsonable_encoder(db_obj)
-        update_data = obj_in.dict(exclude_unset=True)
-
-        if 'full_amount' in update_data:
-            if obj_data["invested_amount"] == update_data["full_amount"]:
-                update_data["fully_invested"] = True
-                update_data["close_date"] = datetime.now()
-
-        for field in obj_data:
-            if field in update_data:
-                setattr(db_obj, field, update_data[field])
-
-        session.add(db_obj)
-        await session.commit()
-        await session.refresh(db_obj)
-        return db_obj
 
     async def get(
             self,
@@ -57,7 +21,7 @@ class CRUDCharityProject(CRUDBase):
 
     async def get_obj_with_same_name(
             self,
-            obj_in: CharityProjectCreate,
+            obj_in,
             session: AsyncSession
     ):
         db_obj = await session.execute(
